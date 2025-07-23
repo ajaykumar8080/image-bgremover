19ec4282001ecc93b47e156693e6cb8bba0d2131
G AJAY KUMAR <154809154+ajaykumar8080@users.noreply.github.com>
2025-07-23 21:49:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, send_file, render_template
+from rembg import remove
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/remove-background', methods=['POST'])
+def remove_background():
+    if 'file' not in request.files:
+        return "No file uploaded", 400
+
+    file = request.files['file']
+    input_image = Image.open(file.stream)
+    output = io.BytesIO()
+
+    # Remove background
+    output_image = remove(input_image)
+    output_image.save(output, format='PNG')
+    output.seek(0)
+
+    # Return the file as an attachment to allow downloading
+    return send_file(output, mimetype='image/png', as_attachment=True, download_name='output_image.png')
+
+if __name__ == '__main__':
+    app.run(debug=True)
